2e6d9a1f20085f211d2cc279216b89ad0d78f4b7
KhristinaVorozhbet93 <vorozhbet.khristina@yandex.ru>
2023-07-09 13:22:30 +0400

    Добавлены запросы для удаления товара из каталога по RPC и REST

--- OnlineShop/Program.cs
+++ OnlineShop/Program.cs
@@ -9,12 +9,13 @@ Catalog catalog = new Catalog();
 app.MapGet("/get_products", GetProducts);
 app.MapGet("/get_product", GetProductById);
 app.MapPost("/add_product", AddProduct);
-
+app.MapPost("/delete_product", DeleteProduct);
 
 //REST
 app.MapGet("/products", GetProducts);
 app.MapGet("/product", GetProductById);
 app.MapPost("/products/product", AddProduct);
+app.MapDelete("/products/{productId}", DeleteProduct);
 
 app.Run();
 
@@ -29,4 +30,8 @@ void AddProduct(Product product)
 Product GetProductById(Guid id)
 {
     return catalog.GetProductById(id);
+}
+void DeleteProduct(Guid productId)
+{
+    catalog.DeleteProduct(productId);
 }
