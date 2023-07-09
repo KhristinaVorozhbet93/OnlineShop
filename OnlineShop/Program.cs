f2a3cc836124fd5cacc957c06303b33c8f31b831
KhristinaVorozhbet93 <vorozhbet.khristina@yandex.ru>
2023-07-09 13:56:24 +0400

    Реализован DI+DIP

--- OnlineShop/Program.cs
+++ OnlineShop/Program.cs
@@ -7,6 +7,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddSingleton<ICatalog, InMemoryCatalog>();
+builder.Services.AddSingleton<IClock, Clock>();
 
 var app = builder.Build();
 app.UseSwagger();
