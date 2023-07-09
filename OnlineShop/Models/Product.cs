de98005a060096b8d58b305a1ea32d7a5f5649b9
KhristinaVorozhbet93 <vorozhbet.khristina@yandex.ru>
2023-07-09 13:04:38 +0400

    Добавьте файлы проекта.

---
+++ OnlineShop/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿namespace OnlineShop.Models
+{
+    public class Product
+    {
+        public Product(string name, decimal price)
+        {
+            ArgumentException.ThrowIfNullOrEmpty(nameof(name));
+            Name = name;
+            Price = price;
+        }
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public DateTime ProducedAt { get; set; }
+        public DateTime ExpiredAt { get; set; }
+    }
+}
