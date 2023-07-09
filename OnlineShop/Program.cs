be3d43be495f258d64c125f71c8b48cb47ea4e64
KhristinaVorozhbet93 <vorozhbet.khristina@yandex.ru>
2023-07-09 13:21:18 +0400

    Добавлены запросы для получения товара по ID из каталога по RPC и REST

--- OnlineShop/Program.cs
+++ OnlineShop/Program.cs
@@ -7,11 +7,13 @@ Catalog catalog = new Catalog();
 
 //RPC
 app.MapGet("/get_products", GetProducts);
+app.MapGet("/get_product", GetProductById);
 app.MapPost("/add_product", AddProduct);
 
 
 //REST
 app.MapGet("/products", GetProducts);
+app.MapGet("/product", GetProductById);
 app.MapPost("/products/product", AddProduct);
 
 app.Run();
@@ -23,4 +25,8 @@ List<Product> GetProducts()
 void AddProduct(Product product)
 {
     catalog.AddProduct(product);
+}
+Product GetProductById(Guid id)
+{
+    return catalog.GetProductById(id);
 }
