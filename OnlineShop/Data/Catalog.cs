02b48f7ee6e0fd0ae8fddacc2d9a782c9dc66251
KhristinaVorozhbet93 <vorozhbet.khristina@yandex.ru>
2023-07-09 13:06:37 +0400

    Добавлен класс Catalog

---
+++ OnlineShop/Data/Catalog.cs
@@ -0,0 +1,52 @@
+﻿using OnlineShop.Models;
+
+namespace OnlineShop.Data
+{
+    public class Catalogcs
+    {
+        private readonly List<Product> _products;
+        private object _productsSyncObj = new();
+
+        public InMemoryCatalog()
+        {
+            _products = GenerateProducts(10);
+        }
+        private static List<Product> GenerateProducts(int count)
+        {
+            var random = new Random();
+            var products = new Product[count];
+
+            var productNames = new string[]
+            {
+            "Молоко",
+            "Хлеб",
+            "Яблоки",
+            "Макароны",
+            "Сахар",
+            "Кофе",
+            "Чай",
+            "Рис",
+            "Масло подсолнечное",
+            "Сыр"
+            };
+
+            for (int i = 0; i < count; i++)
+            {
+                var name = productNames[i];
+                var price = random.Next(50, 500);
+                var producedAt = DateTime.Now.AddDays(-random.Next(1, 30));
+                var expiredAt = producedAt.AddDays(random.Next(1, 365));
+
+
+                products[i] = new Product(name, price)
+                {
+                    Id = Guid.NewGuid(),
+                    Description = "Описание " + name,
+                    ProducedAt = producedAt,
+                    ExpiredAt = expiredAt
+                };
+            }
+            return products.ToList();
+        }
+    }
+}
