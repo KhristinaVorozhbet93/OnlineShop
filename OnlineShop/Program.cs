f30aa07418e1d80d6dd70e1d8f00d35a713c9b32
KhristinaVorozhbet93 <vorozhbet.khristina@yandex.ru>
2023-07-09 13:13:21 +0400

    Добавлены запросы для добавления товара в каталог по RPC и REST

--- OnlineShop/Program.cs
+++ OnlineShop/Program.cs
@@ -7,12 +7,20 @@ Catalog catalog = new Catalog();
 
 //RPC
 app.MapGet("/get_products", GetProducts);
+app.MapPost("/add_product", AddProduct);
+
 
 //REST
 app.MapGet("/products", GetProducts);
+app.MapPost("/products/product", AddProduct);
+
 app.Run();
 
 List<Product> GetProducts()
 {
     return catalog.GetProducts();
+}
+void AddProduct(Product product)
+{
+    catalog.AddProduct(product);
 }
