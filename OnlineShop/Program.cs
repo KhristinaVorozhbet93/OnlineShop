9ee100adc753a15051e1ab2ea3bb7b425136fb2e
KhristinaVorozhbet93 <vorozhbet.khristina@yandex.ru>
2023-07-09 13:23:45 +0400

    Добавлены запросы для редактирования товара в каталоге по RPC и REST

--- OnlineShop/Program.cs
+++ OnlineShop/Program.cs
@@ -10,12 +10,14 @@ app.MapGet("/get_products", GetProducts);
 app.MapGet("/get_product", GetProductById);
 app.MapPost("/add_product", AddProduct);
 app.MapPost("/delete_product", DeleteProduct);
+app.MapPost("/update_product", UpdateProduct);
 
 //REST
 app.MapGet("/products", GetProducts);
 app.MapGet("/product", GetProductById);
 app.MapPost("/products/product", AddProduct);
 app.MapDelete("/products/{productId}", DeleteProduct);
+app.MapPut("/products/{productId}", UpdateProduct);
 
 app.Run();
 
@@ -34,4 +36,8 @@ Product GetProductById(Guid id)
 void DeleteProduct(Guid productId)
 {
     catalog.DeleteProduct(productId);
+}
+void UpdateProduct(Guid productId, Product newProduct)
+{
+    catalog.UpdateProduct(productId, newProduct);
 }
