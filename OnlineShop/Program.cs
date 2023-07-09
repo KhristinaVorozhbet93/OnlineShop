15b5236cd73ec4ac150a88761439de82674d859e
KhristinaVorozhbet93 <vorozhbet.khristina@yandex.ru>
2023-07-09 13:11:48 +0400

    Добавлены запросы для получения товаров из каталога по RPC и REST

--- OnlineShop/Program.cs
+++ OnlineShop/Program.cs
@@ -1,6 +1,18 @@
+using OnlineShop.Data;
+using OnlineShop.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
+Catalog catalog = new Catalog();
 
-app.MapGet("/", () => "Hello World!");
+//RPC
+app.MapGet("/get_products", GetProducts);
 
+//REST
+app.MapGet("/products", GetProducts);
 app.Run();
+
+List<Product> GetProducts()
+{
+    return catalog.GetProducts();
+}
